454c2e9c40b9e778a082c93fb3070beac26c267e
Jibrilzito <101795527+Jibrilzito@users.noreply.github.com>
2023-02-02 18:54:53 +0100

    Numpad halv færdig, animationer halv færdig
    BULLSHIT SKER

--- Assets/Camera And Movement/ThirdPersonMovement.cs
+++ Assets/Camera And Movement/ThirdPersonMovement.cs
@@ -15,7 +15,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        // Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
